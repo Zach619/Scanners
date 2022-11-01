ff1336d967b85b86d7172abc63fa3cc9234de43a
Zach619 <112730663+Zach619@users.noreply.github.com>
2022-11-01 13:59:45 -0700

    Update Scanner.java

--- Scanner.java
+++ Scanner.java
@@ -2,29 +2,18 @@ import java.util.Scanner;
 
 public class MyScanner
 {
-    READ JAVA ORCLE 8 SCANNER
-    READ JAVA ORCLE 8 SCANNER
-    READ JAVA ORCLE 8 SCANNER
-    READ JAVA ORCLE 8 SCANNER
-    READ JAVA ORCLE 8 SCANNER
+
     public void run() {
-        Scanner scanner = new Scanner(System.in); 
-        int a = ("4 29 8 73 10 35");
+        Scanner scanner = new Scanner(System.in);
+        scanner.useDelimiter(",|\n");
         while (true) {
-            if (scanner.hasNext()) {
-                String str = scanner.next();
-                System.out.println(str);
-            }
-            Scanner scanner = new Scanner(a);
-            while (scanner.hasNext()) {
-                String token = scanner.next();
-                System.out.println();
-            }
+            String word = scanner.next();
+            System.out.println(word);
         }
     }
+
     public static void main(String[] args) {
         MyScanner myScanner = new MyScanner();
         myScanner.run();
     }
 }
-
